850d0f896fcc24a0067a9867cb90737af6133cbe
bdwilliamsvanzant <34508649+bdwilliamsvanzant@users.noreply.github.com>
2019-10-19 23:07:00 -0700

    recent drink

--- src/app/recent-drink.service.ts
+++ src/app/recent-drink.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { fullDrink } from './interfaces/fullDrink.interface';
+import { key } from './apikey';
 @Injectable({
   providedIn: 'root'
 })
 export class RecentDrinkService {
-
-  constructor() { }
+  url  : string ="https://www.thecocktaildb.com/api/json/v2/"+ key +"/recent.php";
+  constructor(public http: HttpClient) { }
+  getRecentDrink(){
+    return this.http.get<fullDrink>(this.url);
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { AlertController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { Storage } from '@ionic/storage'
 import { RandomdrinkService } from '../randomdrink.service';
+import { RecentDrinkService } from '../recent-drink.service';
 
 @Component({
   selector: 'app-tab1',
@@ -13,19 +14,23 @@ export class Tab1Page {
   name: string;
   randName: string;
   randImg: string;
-  ingredients: [[string,string]];
-  directions: string;
-  count: number;
-  countList=['1'];
-  strTest: string;
+  randIngredients: [[string,string]];
+  randDirections: string;
+  recName: string;
+  recImg: string;
+  recIngredients: [[string,string]];
+  recDirections: string;
+
   constructor(
     public alertController: AlertController,
     public route: ActivatedRoute,
     public storage: Storage,
-    public rand: RandomdrinkService
+    public rand: RandomdrinkService,
+    public rec: RecentDrinkService
   ) {
     this.getName()
     this.randomDrink();
+    this.recentDrink();
   }
   
 
@@ -35,29 +40,51 @@ export class Tab1Page {
       this.name = val;
     });
   }
+  recentDrink(){
+    this.rec.getRecentDrink().subscribe(data =>{
+      console.log(data);
+      this.recName = data.drinks[0].strDrink;
+      this.recImg = data.drinks[0].strDrinkThumb;
+      this.recIngredients = [[null,null]];
+      this.recIngredients.push([data.drinks[0].strMeasure1,data.drinks[0].strIngredient1]);
+      this.recIngredients.push([data.drinks[0].strMeasure2,data.drinks[0].strIngredient2]);
+      this.recIngredients.push([data.drinks[0].strMeasure3,data.drinks[0].strIngredient3]);
+      this.recIngredients.push([data.drinks[0].strMeasure4,data.drinks[0].strIngredient4]);
+      this.recIngredients.push([data.drinks[0].strMeasure5,data.drinks[0].strIngredient5]);
+      this.recIngredients.push([data.drinks[0].strMeasure6,data.drinks[0].strIngredient6]);
+      this.recIngredients.push([data.drinks[0].strMeasure7,data.drinks[0].strIngredient7]);
+      this.recIngredients.push([data.drinks[0].strMeasure8,data.drinks[0].strIngredient8]);
+      this.recIngredients.push([data.drinks[0].strMeasure9,data.drinks[0].strIngredient9]);
+      this.recIngredients.push([data.drinks[0].strMeasure10,data.drinks[0].strIngredient10]);
+      this.recIngredients.push([data.drinks[0].strMeasure11,data.drinks[0].strIngredient11]);
+      this.recIngredients.push([data.drinks[0].strMeasure12,data.drinks[0].strIngredient12]);
+      this.recIngredients.push([data.drinks[0].strMeasure13,data.drinks[0].strIngredient13]);
+      this.recIngredients.push([data.drinks[0].strMeasure14,data.drinks[0].strIngredient14]);
+      this.recIngredients.push([data.drinks[0].strMeasure15,data.drinks[0].strIngredient15]);  
+      this.recDirections =data.drinks[0].strInstructions;
+    })
+  }
   randomDrink(){
-    this.count = 1;
     this.rand.getRandDrink().subscribe(data =>{
       this.randName = data.drinks[0].strDrink;
       this.randImg = data.drinks[0].strDrinkThumb;
-      this.ingredients = [[null,null]];
-      this.ingredients.push([data.drinks[0].strMeasure1,data.drinks[0].strIngredient1]);
-      this.ingredients.push([data.drinks[0].strMeasure2,data.drinks[0].strIngredient2]);
-      this.ingredients.push([data.drinks[0].strMeasure3,data.drinks[0].strIngredient3]);
-      this.ingredients.push([data.drinks[0].strMeasure4,data.drinks[0].strIngredient4]);
-      this.ingredients.push([data.drinks[0].strMeasure5,data.drinks[0].strIngredient5]);
-      this.ingredients.push([data.drinks[0].strMeasure6,data.drinks[0].strIngredient6]);
-      this.ingredients.push([data.drinks[0].strMeasure7,data.drinks[0].strIngredient7]);
-      this.ingredients.push([data.drinks[0].strMeasure8,data.drinks[0].strIngredient8]);
-      this.ingredients.push([data.drinks[0].strMeasure9,data.drinks[0].strIngredient9]);
-      this.ingredients.push([data.drinks[0].strMeasure10,data.drinks[0].strIngredient10]);
-      this.ingredients.push([data.drinks[0].strMeasure11,data.drinks[0].strIngredient11]);
-      this.ingredients.push([data.drinks[0].strMeasure12,data.drinks[0].strIngredient12]);
-      this.ingredients.push([data.drinks[0].strMeasure13,data.drinks[0].strIngredient13]);
-      this.ingredients.push([data.drinks[0].strMeasure14,data.drinks[0].strIngredient14]);
-      this.ingredients.push([data.drinks[0].strMeasure15,data.drinks[0].strIngredient15]);  
-      console.log(this.ingredients);
-      this.directions =data.drinks[0].strInstructions;
+      this.randIngredients = [[null,null]];
+      this.randIngredients.push([data.drinks[0].strMeasure1,data.drinks[0].strIngredient1]);
+      this.randIngredients.push([data.drinks[0].strMeasure2,data.drinks[0].strIngredient2]);
+      this.randIngredients.push([data.drinks[0].strMeasure3,data.drinks[0].strIngredient3]);
+      this.randIngredients.push([data.drinks[0].strMeasure4,data.drinks[0].strIngredient4]);
+      this.randIngredients.push([data.drinks[0].strMeasure5,data.drinks[0].strIngredient5]);
+      this.randIngredients.push([data.drinks[0].strMeasure6,data.drinks[0].strIngredient6]);
+      this.randIngredients.push([data.drinks[0].strMeasure7,data.drinks[0].strIngredient7]);
+      this.randIngredients.push([data.drinks[0].strMeasure8,data.drinks[0].strIngredient8]);
+      this.randIngredients.push([data.drinks[0].strMeasure9,data.drinks[0].strIngredient9]);
+      this.randIngredients.push([data.drinks[0].strMeasure10,data.drinks[0].strIngredient10]);
+      this.randIngredients.push([data.drinks[0].strMeasure11,data.drinks[0].strIngredient11]);
+      this.randIngredients.push([data.drinks[0].strMeasure12,data.drinks[0].strIngredient12]);
+      this.randIngredients.push([data.drinks[0].strMeasure13,data.drinks[0].strIngredient13]);
+      this.randIngredients.push([data.drinks[0].strMeasure14,data.drinks[0].strIngredient14]);
+      this.randIngredients.push([data.drinks[0].strMeasure15,data.drinks[0].strIngredient15]);  
+      this.randDirections =data.drinks[0].strInstructions;
     })
   }
   ngOnInit(){
